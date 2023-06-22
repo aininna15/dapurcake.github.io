a9cbffe187665d33d3627f0b1226c6808d6e8b79
aininna15 <137372362+aininna15@users.noreply.github.com>
2023-06-22 12:27:44 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #0665ad;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #0665ad"><b>Dapur Cake</b></h3>
+					<p>Jl. Poros Camba-Maros</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6282349263515</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> dapurcake@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #0665ad; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; Dapur Cake</span>
+		</div>
+	</footer>
+
+</body>
+</html>
